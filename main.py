31c107edc8ca3456c4fbd123769dff170f57b3a9
Jean Sousa <jean.sousa@vexpenses.com>
2026-02-06 23:37:24 -0300

    add class Item that inherits Basemodel class from pydantic

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 # instancia do fast api para criar os endpoints
 app = FastAPI()
+
+# links de acesso as docs swagger e redock
+# http://127.0.0.1:8000/docs
+# http://127.0.0.1:8000/redoc
+
+
+# classe que herda BaseModel do pydantic 
+# além de validar os tipos ela lê os TYPE HINTS e consegue criar o constructor sozinha
+class Item(BaseModel):
+    name: str 
+    price: float 
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -16,3 +30,8 @@ def read_root():
 # segundo argumento é um query param 'q' opcional do tipo union de str ou None com valor default None
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
